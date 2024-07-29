29456de383c92f5f1fcc32baf97c9a2d23ebdb1c
AndileMngomezulu <andymdlalose1@gmail.com>
2024-07-29 11:43:08 -0700

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -26,10 +26,8 @@ if __name__ == "__main__":
     todos = todos_response.json()
 
     with open("{}. csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, qouting=CSV.QOUTE_ALL)
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
         for todo in todos:
             writer.writerow([user_id, username, todo.get("completed"),
                              todo.get("title")])
-
-
